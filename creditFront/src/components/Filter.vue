fcf722bffd47bf8edc25bf8bcd15b28e7a8857c5
jeffreyyang3 <jeffreyhasmail@gmail.com>
2019-09-01 14:44:36 -0700

    current

--- creditFront/src/components/Filter.vue
+++ creditFront/src/components/Filter.vue
@@ -1,11 +1,22 @@
 <template>
   <div class="filterContainer">
-    <select v-model="selected">
+    <div class="filterName">{{ name }}</div>
+    <select class="form-control filterOptions" v-model="selected">
       <option value="#noFilter">No Filter</option>
       <option v-for="option in options" :value="option" :key="Math.random() + option ">{{ option }}</option>
     </select>
   </div>
 </template>
+
+<style scoped>
+.filterContainer {
+  display: flex;
+}
+
+.filterOptions {
+  width: 25%;
+}
+</style>
 
 
 <script>
