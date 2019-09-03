4ce5612afd7cd100899d0ed6876b8bf8ab7aa2f9
jeffreyyang3 <jeffreyhasmail@gmail.com>
2019-09-02 23:52:55 -0700

    vue selects

--- creditFront/src/components/typeAhead.vue
+++ creditFront/src/components/typeAhead.vue
@@ -7,14 +7,21 @@
       type="text"
       v-model="currentTyped"
     />
+    <div class="allSelects">
+      <div v-for="filter in allFilters" :key="filter.displayName" class="vueSelectWrapper">
+        <h5>{{ filter.displayName }}</h5>
+        <VueSelect :options="filter.options" @input="handleOptionChange(filter)($event)" />
+      </div>
+    </div>
+
     <!-- <div v-for="filter in allFilters" :key="filter.displayName">{{ filter }}</div> -->
-    <cardFilter
+    <!-- <cardFilter
       v-for="filter in allFilters"
       :name="filter.displayName"
       :options="filter.options"
       :key="filter.displayName"
       v-on:optionChange="handleOptionChange(filter)($event)"
-    />
+    />-->
     <!-- <div v-for="card in filteredCards" :key="card.cardKey"> {{ card.displayName }}</div> -->
 
     <div class="suggestions" v-if="showSuggestions && !forRemove">
@@ -62,8 +69,19 @@ $hotBoxShadow: 0 2px 4px 0 rgba(34, 36, 38, 0.12),
   0 2px 10px 0 rgba(34, 36, 38, 0.15);
 $border: 1px solid #d4d4d5;
 $borderRadius: 8px;
-.noCards {
+
+.vueSelectWrapper {
+  display: flex;
+  width: 100%;
+  align-items: baseline;
+  justify-content: space-between;
+  margin-top: 10px;
+  margin-bottom: 10px;
 }
+.v-select {
+  min-width: 30%;
+}
+
 .typeAhead {
   margin: 2%;
 }
@@ -119,17 +137,19 @@ $borderRadius: 8px;
 </style>
 <script type="text/javascript">
 import { mapState } from "vuex";
-import cardFilter from "@/components/Filter";
 import cardSelectComponent from "@/components/cardSelectComponent";
 import cardFilters from "@/data/cardFilters";
+import VueSelect from "vue-select";
+import "vue-select/dist/vue-select.css";
 export default {
   name: "typeAhead",
-  components: { cardSelectComponent, cardFilter },
+  components: { cardSelectComponent, VueSelect },
   data: function() {
     return {
       currentTyped: "",
       activeFilters: [],
-      filterState: {}
+      filterState: {},
+      noFilter: "No Filterdd"
     };
   },
   props: {
@@ -228,8 +248,9 @@ export default {
     // />
     handleOptionChange: function(filter) {
       return value => {
+        console.log(value, "val");
         this.$set(this.filterState, filter.displayName, card => {
-          if (value === "No Filter") return () => true;
+          if (value === "No Filter" || value === null) return () => true;
           return filter.filterFn(card, value);
         });
       };
