9ca15aab6cd35c73a6ca60ceadaefc62248c73d2
Jeffrey Yang <jeffreyyang@Jeffreys-MacBook-Air.local>
2019-09-13 00:39:38 -0700

    text content

--- creditFront/src/views/aboutStuff/siteInfo.vue
+++ creditFront/src/views/aboutStuff/siteInfo.vue
@@ -3,21 +3,52 @@
     <h1>I made this site for a few reasons.</h1>
     <h3>I like credit cards a lot</h3>
     <p>
-      Ever since recently discovering the subreddit r/churning, I've always
+      Ever since recently discovering the subreddit
+      <a
+        href="https://www.reddit.com/r/churning/"
+        target="_blank"
+      >r/churning</a>, I've always
       been fascinated by credit card rewards. Free money and cute looking
       cards! Kinda like Pokémon cards, but with no skill requirement to
       make money with them. It had to be too good to be true. After a bit more life experience
       and research, I'm happy to report that it really is that good, unless you
       carry a balance/pay late.
       Shoutout to the US's legendary culture of spending/lending/debt management
-      for enabling this. I don't think any other country's credit cards even come
+      for making this all possible. I don't think any other country's credit cards even come
       close.
     </p>
-    <h3>There are a lot of nice cards out there</h3>
-    <p></p>
+    <h3>There are a lot of good credit cards</h3>
+    <p>
+      It can be really time-consuming to sift through the options to find
+      whatever cards benefit you the most. I can relate, I don't even want to
+      think about how much time I spent reading credit card forums and subreddits.
+      Dividing the amount of rewards I've earned vs. that time probably yields a
+      very poor hourly rate. I'm fine with it though, it's a hobby and most hobbies
+      tend to do pretty badly in that regard.
+    </p>
 
-    <h3>I'm lazy</h3>
-    <p></p>
+    <p>
+      But for those who don't like putting in a lot of time (almost everyone else),
+      I feel like the time I put in finding worthwhile cards can save you a lot of
+      time in seeing what fits your spending best.
+    </p>
+
+    <h3>Comparison should be easy, personalized, and unbiased</h3>
+    <p>
+      If you google search "compare credit cards", everything is either:
+      <br />
+      <br />1: a bank
+      <br />2: NerdWallet type site
+      <br />
+      <br />All definitely useful, they give more info and look a lot better than my site.
+      But all of them (or at least all of them on the first 3 pages of that google search) basically
+      have no meaningful personalization other than "what's your credit score" and "travel rewards or
+      cash back". I figured
+    </p>
+    <p>
+      Almost all of these are also biased. The banks obviously only advertising their own
+      cards, but the other websites focus on the
+    </p>
   </div>
 </template>
 <script>
